25e0bdf0857b479682abf02f7ca83d25714dd062
Oleh <oleh@localhost.localdomain>
2015-03-31 21:44:59 +0300

    Added underscore to deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'oleh:foursquare',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for searching places with Foursquare API',
   // URL to the Git repository containing the source code for this package.
